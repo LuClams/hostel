8776491d78d68b893c1b33a6b5566439c26f6c46
Lunel Clamy <lunelclamy@hotmail.fr>
2022-04-17 20:06:50 +0200

    Commit personnalisation du dashboard easyAdmin avec contact hostel room et user

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,9 +2,16 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Administrator;
+use App\Entity\Contact;
+use App\Entity\Hostel;
+use App\Entity\Manager;
+use App\Entity\Room;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,12 +20,12 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        //return parent::index();
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+         $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+         return $this->redirect($adminUrlGenerator->setController(HostelCrudController::class)->generateUrl());
 
         // Option 2. You can make your dashboard redirect to different pages depending on the user
         //
@@ -40,7 +47,20 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        //yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linktoRoute('Back to the website', 'fas fa-home', 'app_home');
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+
+        yield MenuItem::section('Managemement');
+        //yield MenuItem::linkToCrud('Managers', 'fa fa-file-text', Manager::class);
+        yield MenuItem::linkToCrud('Contact Form', 'fa fa-user', Contact::class);
+
+        yield MenuItem::section('Users');
+        yield MenuItem::linkToCrud('Users', 'fa fa-comment', User::class);
+
+        yield MenuItem::section('Hostel Group');
+        yield MenuItem::linkToCrud('Hostels', 'fa fa-comment', Hostel::class);
+        yield MenuItem::linkToCrud('Rooms', 'fa fa-comment', Room::class);
     }
 }
