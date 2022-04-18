154b0c3a732ba067fa3d0e9d6b09571f9d65be58
Lunel Clamy <lunelclamy@hotmail.fr>
2022-04-18 12:32:54 +0200

    Commit remplissage de l'entity User et de UserCrudController.php, redirection vers la home après connexion du user, remplissage de l'entity hostel et de HostelCrudController.php

--- src/Controller/Admin/HostelCrudController.php
+++ src/Controller/Admin/HostelCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Hostel;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class HostelCrudController extends AbstractCrudController
 {
@@ -12,14 +15,17 @@ class HostelCrudController extends AbstractCrudController
         return Hostel::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
+            //IdField::new('id'),
+            TextField::new('name'),
+            TextField::new('city'),
+            TextField::new('address'),
             TextEditorField::new('description'),
+            AssociationField::new('room')
         ];
     }
-    */
+
 }
--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,7 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use DateTime;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\DateTimeFilter;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -12,14 +18,16 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            //IdField::new('id'),
+            EmailField::new('email'),
+            TextField::new('firstname'),
+            TextField::new('lastname'),
+            TimeField::new('booking')
         ];
     }
-    */
+
 }
